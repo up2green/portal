10121b67ab5d17f7a30ec4a8b2ada64b9b91b829
Clément Gautier <clement.gautier.76@gmail.com>
2014-09-06 14:30:32 +0200

    Added bower / favicon / main bundle and route

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -14,6 +14,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Up2green\Bundle\PortalBundle\Up2greenPortalBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Up2green/Bundle/PortalBundle/Up2greenPortalBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Up2green\Bundle\PortalBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class Up2greenPortalBundle extends Bundle
+{
+}
