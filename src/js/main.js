c0a5309c9289fcae495d0b6ca41d998e48a7a4f2
Clément Gautier <clement.gautier.76@gmail.com>
2015-02-09 22:08:40 +0100

    Looks better

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,17 @@
 angular.module('app', ['duParallax']).
     controller('IndexController', function($scope, parallaxHelper){
-        $scope.background = parallaxHelper.createAnimator(-0.3);
+        $scope.background = function(elementPosition) {
+            var factor = -0.4;
+            var pos = Math.min(Math.max(elementPosition.elemY*factor, 0), 255);
+            var bg = 255-pos;
+            return {
+                backgroundPosition: pos + 'px ' + pos + 'px',
+                color: 'rgb(' + bg + ', ' + bg + ', ' + bg + ')'
+            };
+        }
+
+        $scope.logo = parallaxHelper.createAnimator(-0.5);
+        $scope.sectionImageOdd = parallaxHelper.createAnimator(0.5, null, 0, -100);
+        $scope.sectionImageEven = parallaxHelper.createAnimator(-0.5, 0, null, -400);
     }
 );
