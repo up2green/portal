10634b1b2a61bf7d9daa3b71baceec99c2fb1e22
Clément Gautier <clement.gautier.76@gmail.com>
2015-02-10 21:10:41 +0100

    Removed some commited vendors and integrated youtube

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var minifyHtml = require('gulp-minify-html');
 var minifyCss = require('gulp-minify-css');
 var rev = require('gulp-rev');
 
-gulp.task('usemin', function () {
+gulp.task('build', function () {
     return gulp.src('src/index.html')
         .pipe(usemin({
             css: [minifyCss(), 'concat'],
--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,23 @@
-angular.module('app', ['duParallax']).
-    controller('IndexController', function($scope, parallaxHelper){
+angular
+    .module('app', ['duParallax'])
+    .controller('IndexController', ['$scope', 'parallaxHelper', function($scope, parallaxHelper){
         $scope.background = function(elementPosition) {
             var factor = -0.4;
             var pos = Math.min(Math.max(elementPosition.elemY*factor, 0), 255);
             var bg = 255-pos;
             return {
-                backgroundPosition: pos + 'px ' + pos + 'px',
-                color: 'rgb(' + bg + ', ' + bg + ', ' + bg + ')'
+                backgroundPosition: pos + 'px ' + pos + 'px'
             };
         }
+
+        $scope.fadeOut = function(elementPosition) {
+            var factor = -0.4;
+            var pos = Math.min(Math.max(elementPosition.elemY*factor, 0), 200);
+
+            return 1-(pos/200);
+        };
 
         $scope.logo = parallaxHelper.createAnimator(-0.5);
         $scope.sectionImageOdd = parallaxHelper.createAnimator(0.5, null, 0, -100);
         $scope.sectionImageEven = parallaxHelper.createAnimator(-0.5, 0, null, -400);
-    }
-);
+    }]);
